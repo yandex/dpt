7701b58414d626d4620f6023641c55d5d8f32a5c
Artem Shitov <artemshitov@yandex-team.ru>
2017-03-13 02:47:15 +0300

    Allow pipelining compilers

--- src/server/routes/render.js
+++ src/server/routes/render.js
@@ -1,9 +1,28 @@
 import Path from 'path';
 
 import _ from 'lodash';
+import Promise from 'bluebird';
 
+import * as File from '../file';
 import Cache from '../../cache';
 import logger from '../../logger';
+
+function compile(compilers, options) {
+    let input = await File.read(options.path);
+    let init = {
+        content: {
+            body: input
+        },
+        dependencies: []
+    };
+    return Promise.reduce(compilers, async (acc, c) => {
+        let result = await c(acc.content.body, options);
+        return {
+            content: result.content,
+            dependencies: _.union(acc.dependencies, result.dependencies)
+        };
+    });
+}
 
 export default function render(compilers) {
     const cache = new Cache();
@@ -17,10 +36,10 @@ export default function render(compilers) {
 
         let comp = _.find(compilers, c => c.test.test(path));
 
-        if (comp) {
+        if (comp && (comp.compilers || comp.compiler)) {
             try {
-                let { compiler } = comp;
-                let { body, mime } = await cache.cached(compiler)(options);
+                let compilers = comp.compilers || [comp.compiler];
+                let { body, mime } = await cache.cached(compiler(compilers, options));
                 res.type(mime).send(body);
             } catch (err) {
                 res.status(500).send(`Error: ${err.message}`);

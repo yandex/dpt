e0b89eb24e943c9a5a9d391ca279ea3f1ffb9013
Artem Shitov <artemshitov@yandex-team.ru>
2017-08-10 15:34:25 +0300

    Refactoring

--- src/loader/platform.js
+++ src/loader/platform.js
@@ -2,40 +2,45 @@ import Qs from 'qs';
 
 export default class DepotPlatform {
     constructor() {
+        this.platform = qsPlatform() || fnPlatform() || uaPlatform() || 'desktop';
+    }
+
+    qsPlatform() {
         let qs = Qs.parse(window.location.search.substr(1));
+        return qs.platform || qs.p;
+    }
+
+    fnPlatform() {
         let pathname = window.location.pathname;
         let pathParts = pathname.split('/');
         let filenameParts = pathParts[pathParts.length - 1].split('.');
 
-        let qsPlatform = qs.platform || qs.p;
         let fnPlatform;
 
         if (/\.html$/.test(pathname) && filenameParts.length > 2 && filenameParts[1] !== 'bml') {
             fnPlatform = filenameParts[1];
         }
 
-        // Detect platform from userAgent
-        let uaPlatform;
+        return fnPlatform;
+    }
+
+    uaPlatform() {
         let userAgent = window.navigator.userAgent;
         if (/iPhone/.test(userAgent)) {
-            uaPlatform = 'mobile';
+            return 'mobile';
         } else if (/iPad/.test(userAgent)) {
-            uaPlatform = 'tablet';
+            return 'tablet';
         } else if (/Android/.test(userAgent) || window.screen.availWidth <= 1024) {
-            let minRez = Math.min(window.screen.availHeight, window.screen.availWidth);
-            if (minRez < 768) {
-                uaPlatform = 'mobile';
+            if (Math.min(window.screen.availHeight, window.screen.availWidth) < 768) {
+                return 'mobile';
             } else {
-                uaPlatform = 'tablet';
+                return 'tablet';
             }
         }
-
-        this.platform = qsPlatform || fnPlatform || uaPlatform || 'desktop';
     }
 
     is(platform) {
-        return this.platform === platform
-            || this.platform === platform + '-experimental';
+        return this.platform === platform || this.platform === platform + '-experimental';
     }
 
     isDesktop() {

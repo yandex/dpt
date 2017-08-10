5df819ea618e320b0c745cc2d8a6bb0fbcf5d82f
Artem Shitov <artemshitov@yandex-team.ru>
2017-08-10 14:49:56 +0300

    Try to use the user's version of Beast

--- src/compilers/bml.js
+++ src/compilers/bml.js
@@ -1,4 +1,12 @@
-import '../vendor/beast';
+import * as Path from 'path';
+
+let Beast;
+
+try {
+    Beast = require(Path.join(process.cwd(), '/vendor/beast'));
+} catch (e) {
+    Beast = require('../vendor/beast');
+}
 
 export default function bml(input, {path}) {
     return {
